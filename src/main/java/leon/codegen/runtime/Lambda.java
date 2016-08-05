c1d2f1a8df2ae16c8f20086954ad3cfea00c158c
Manos Koukoutos <emmanouil.koukoutos@epfl.ch>
2016-08-05 17:11:26 +0200

    Delete all java sources

--- src/main/java/leon/codegen/runtime/Lambda.java
+++
@@ -1,7 +0,0 @@
-/* Copyright 2009-2016 EPFL, Lausanne */
-
-package leon.codegen.runtime;
-
-public abstract class Lambda {
-  public abstract Object apply(Object[] args) throws LeonCodeGenRuntimeException;
-}
