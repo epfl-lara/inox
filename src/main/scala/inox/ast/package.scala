9b0f0dc6ba69433e139558c823fd5ca21e25b7e0
Manos Koukoutos <emmanouil.koukoutos@epfl.ch>
2016-08-09 15:51:52 +0200

    Comments

--- src/main/scala/inox/ast/package.scala
+++ src/main/scala/inox/ast/package.scala
@@ -2,21 +2,10 @@
 
 package inox
 
-/** Provides AST definitions for Leon programs.
+/** Provides AST definitions for Inox.
   *
-  * The core language supported by Leon is called Pure Scala and its 
-  * [[leon.purescala.Definitions]] and [[leon.purescala.Expressions]] are defined here.
-  * This package also contains the [[leon.purescala.Types]] definitions. Each of those
-  * trees come with a corresponding set of operations in the ???Ops objects.
-  *
-  * The package also provides general utilities operations on Pure Scala programs, such as
-  * a method lifting phase [[leon.purescala.MethodLifting]] (transforming methods into 
-  * top level functions) and a function closure phase [[leon.purescala.FunctionClosure]]
-  * (lifting an inner function to the top level).
-  *
-  * Two printers for Pure Scala programs are also provided, a [[leon.purescala.PrettyPrinter]]
-  * that outputs a nice and readable program (typically using unicode for some operations) and
-  * a [[leon.purescala.ScalaPrinter]] that outputs a valid Scala program from a Leon 
-  * representation.
+  * The core language supported by Inox is defined in [[inox.ast.Expressions]],
+  * [[inox.ast.Definitions]] and [[inox.ast.Types]]. The trait [[inox.ast.Trees]] provides
+  * all these definitions and one of the integral parts of a [[Program]].
   */
 package object ast {}
