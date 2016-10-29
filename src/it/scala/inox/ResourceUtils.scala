892b7e573af10092f43e20801d46299287f5af81
Nicolas Voirol <voirol.nicolas@gmail.com>
2016-10-29 14:36:24 +0200

    Correct sorting for resource listing

--- src/it/scala/inox/ResourceUtils.scala
+++ src/it/scala/inox/ResourceUtils.scala
@@ -24,7 +24,7 @@ trait ResourceUtils {
         }
       }
 
-      rec(baseDir).filter(f => filter(f.getPath)).toSeq.sortBy(_.getPath)
+      rec(baseDir).filter(f => filter(f.getPath)).toSeq.sortBy(_.getPath).reverse
     }
   }
 }
