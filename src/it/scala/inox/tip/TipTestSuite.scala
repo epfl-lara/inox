8954f4699e31598ae48b0cd56dec638810ae86cb
Nicolas Voirol <voirol.nicolas@gmail.com>
2017-01-16 10:10:19 +0100

    Ignore flaky test (flakiness due to z3 binary)

--- src/it/scala/inox/tip/TipTestSuite.scala
+++ src/it/scala/inox/tip/TipTestSuite.scala
@@ -17,13 +17,20 @@ class TipTestSuite extends TestSuite with ResourceUtils {
     "solver=" + options.findOptionOrDefault(optSelectedSolvers).head
   }
 
+  private def ignoreFile(solver: String, fileName: String): Boolean = {
+    // test containing list of booleans, so CVC4 will crash on this
+    // See http://church.cims.nyu.edu/bugzilla3/show_bug.cgi?id=500
+    (solver == "smt-cvc4" && fileName.endsWith("List-fold.tip")) ||
+    // Z3 binary will predictably segfault on certain permutations of this problem
+    (solver == "smt-z3" && fileName.endsWith("MergeSort2.scala-1.tip"))
+  }
+
   for (file <- resourceFiles("regression/tip/SAT", _.endsWith(".tip"))) {
-    test(s"SAT - ${file.getName}", ctx => if (
-      // XXX: test containing list of booleans, so CVC4 will crash on this
-      //      See http://church.cims.nyu.edu/bugzilla3/show_bug.cgi?id=500
-      ctx.options.findOptionOrDefault(optSelectedSolvers) == Set("smt-cvc4") &&
-      file.getName.endsWith("List-fold.tip")
-    ) Ignore else Test) { ctx =>
+    test(s"SAT - ${file.getName}", ctx =>
+      ctx.options.findOptionOrDefault(optSelectedSolvers).headOption match {
+        case Some(solver) if ignoreFile(solver, file.getName) => Ignore
+        case _ => Test
+    }) { ctx =>
       for ((syms, expr) <- new Parser(file).parseScript) {
         val program = InoxProgram(ctx, syms)
         assert(SimpleSolverAPI(SolverFactory.default(program)).solveSAT(expr).isSAT)
@@ -32,7 +39,11 @@ class TipTestSuite extends TestSuite with ResourceUtils {
   }
 
   for (file <- resourceFiles("regression/tip/UNSAT", _.endsWith(".tip"))) {
-    test(s"UNSAT - ${file.getName}") { ctx =>
+    test(s"UNSAT - ${file.getName}", ctx =>
+      ctx.options.findOptionOrDefault(optSelectedSolvers).headOption match {
+        case Some(solver) if ignoreFile(solver, file.getName) => Ignore
+        case _ => Test
+    }) { ctx =>
       for ((syms, expr) <- new Parser(file).parseScript) {
         val program = InoxProgram(ctx, syms)
         assert(SimpleSolverAPI(SolverFactory.default(program)).solveSAT(expr).isUNSAT)
