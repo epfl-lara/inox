c83e148937d759f630382e9b3dfe970eca4fe94e
Nicolas Voirol <voirol.nicolas@gmail.com>
2018-04-10 15:03:28 +0200

    Add serialization tests for unregistered case classes

--- src/it/scala/inox/tip/TipSerializationSuite.scala
+++ src/it/scala/inox/tip/TipSerializationSuite.scala
@@ -5,7 +5,7 @@ package tip
 
 import org.scalatest._
 
-class TipSerializationSuite extends FunSuite with ResourceUtils {
+class TipSerializationSuite extends FunSpec with ResourceUtils {
   import inox.trees._
 
   val ctx = TestContext.empty
@@ -14,20 +14,40 @@ class TipSerializationSuite extends FunSuite with ResourceUtils {
     f.getParentFile.getName -> f
   }
 
+  // We have to be careful what we unregister as not all `classSerializers` are case classes
+  class ProductSerializer(trees: ast.Trees) extends  utils.InoxSerializer(trees, serializeProducts = true) {
+    override protected def classSerializers =
+      super.classSerializers.filterNot(p => 30 <= p._2.id && p._2.id <= 40)
+  }
+
+  def checkSerializer(
+    serializer: utils.Serializer { val trees: inox.trees.type },
+    program: Program { val trees: inox.trees.type },
+    expr: Expr
+  ) = {
+    val out = new java.io.ByteArrayOutputStream
+    serializer.serializeSymbols(program.symbols, out)
+    serializer.serialize(expr, out)
+
+    val in = new java.io.ByteArrayInputStream(out.toByteArray)
+    val newSymbols = serializer.deserializeSymbols(in)
+    val newExpr = serializer.deserialize[Expr](in)
+
+    assert(program.symbols == newSymbols)
+    assert(expr == newExpr)
+  }
+
   for ((cat, file) <- filesWithCat) {
-    test(s"Serializing/deserializing file $cat/${file.getName}") {
-      val serializer = utils.Serializer(inox.trees)
-      for ((program, expr) <- new Parser(file).parseScript) {
-        val out = new java.io.ByteArrayOutputStream
-        serializer.serializeSymbols(program.symbols, out)
-        serializer.serialize(expr, out)
+    describe(s"Serializing/deserializing file $cat/${file.getName}") {
+      it("with registered classes") {
+        val serializer = utils.Serializer(inox.trees)
+        for ((program, expr) <- new Parser(file).parseScript) checkSerializer(serializer, program, expr)
+      }
 
-        val in = new java.io.ByteArrayInputStream(out.toByteArray)
-        val newSymbols = serializer.deserializeSymbols(in)
-        val newExpr = serializer.deserialize[Expr](in)
-
-        assert(program.symbols == newSymbols)
-        assert(expr == newExpr)
+      it("with unregistered classes") {
+        val serializer = new ProductSerializer(inox.trees)
+          .asInstanceOf[utils.Serializer { val trees: inox.trees.type }]
+        for ((program, expr) <- new Parser(file).parseScript) checkSerializer(serializer, program, expr)
       }
     }
   }
