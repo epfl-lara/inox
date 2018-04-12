5cb98affdb709557ddb2f4f93c8a57872bd0ec79
Nicolas Voirol <voirol.nicolas@gmail.com>
2018-04-12 10:34:55 +0200

    Test for serialization determinism

--- src/it/scala/inox/tip/TipSerializationSuite.scala
+++ src/it/scala/inox/tip/TipSerializationSuite.scala
@@ -31,12 +31,19 @@ class TipSerializationSuite extends FunSpec with ResourceUtils {
     serializer.serialize(program.symbols, out)
     serializer.serialize(expr, out)
 
-    val in = new java.io.ByteArrayInputStream(out.toByteArray)
+    val outBytes = out.toByteArray
+    val in = new java.io.ByteArrayInputStream(outBytes)
     val newSymbols = serializer.deserialize[Symbols](in)
     val newExpr = serializer.deserialize[Expr](in)
 
     assert(program.symbols == newSymbols)
     assert(expr == newExpr)
+
+    val out2 = new java.io.ByteArrayOutputStream
+    serializer.serialize(newSymbols, out2)
+    serializer.serialize(newExpr, out2)
+    val out2Bytes = out2.toByteArray
+    assert(java.util.Arrays.equals(outBytes, out2Bytes))
   }
 
   for ((cat, file) <- filesWithCat) {
