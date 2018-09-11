34535755d924f116a4215206f1e5d7e26fbd1c1a
Nicolas Voirol <voirol.nicolas@gmail.com>
2018-09-11 21:32:35 +0200

    More support for unsigned BV in expr parser

--- src/main/scala/inox/parsing/TypeDeconstructor.scala
+++ src/main/scala/inox/parsing/TypeDeconstructor.scala
@@ -10,15 +10,18 @@ trait TypeDeconstructors extends IRs {
     import TypeIR._
 
     object BVType {
-      def apply(size: Int): String = {
+      def apply(signed: Boolean, size: Int): String = {
         require(size > 0)
 
-        "Int" + size
+        if (signed) "Int" + size
+        else "UInt" + size
       }
 
       def unapply(name: String): Option[(Boolean,Int)] = {
         if (name.startsWith("Int")) {
           scala.util.Try(name.drop(3).toInt).toOption.filter(_ > 0).map(i => (true,i))
+        } else if (name.startsWith("UInt")) {
+          scala.util.Try(name.drop(4).toInt).toOption.filter(_ > 0).map(i => (false,i))
         } else {
           None
         }
