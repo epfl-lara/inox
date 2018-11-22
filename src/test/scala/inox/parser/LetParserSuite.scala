8207d12e6a1b66ab0f03f40840ee91e306230069
Romain Edelmann <romain.edelmann@gmail.com>
2018-11-22 15:25:29 +0100

    Add more tests for lets

--- src/test/scala/inox/parser/LetParserSuite.scala
+++ src/test/scala/inox/parser/LetParserSuite.scala
@@ -34,4 +34,27 @@ class LetParserSuite extends FunSuite {
     assert(l.value == StringLiteral("Hello World"))
     assert(l.body == StringLength(l.vd.toVariable))
   }
+
+  test("Multiple lets.") {
+    val e = e"""
+      let x = let z = 3; z + z;
+      let y = x;
+
+      x + y
+    """
+
+    e match {
+      case Let(x, Let(z, IntegerLiteral(_), Plus(z1, z2)), Let(y, x1, Plus(x2, y1))) => {
+        assert(x.id.name == "x")
+        assert(y.id.name == "y")
+        assert(z.id.name == "z")
+        assert(x.toVariable == x1)
+        assert(x.toVariable == x2)
+        assert(y.toVariable == y1)
+        assert(z.toVariable == z1)
+        assert(z.toVariable == z2)
+      }
+      case _ => fail("Did not match.")
+    }
+  }
 }
