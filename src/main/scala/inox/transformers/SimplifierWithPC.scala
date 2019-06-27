a6cbf8e2488fd2d3aa941963fca65eb5d370c698
Nicolas Voirol <voirol.nicolas@gmail.com>
2019-06-27 22:35:14 +0200

    Simplify inside choose predicates

--- src/main/scala/inox/transformers/SimplifierWithPC.scala
+++ src/main/scala/inox/transformers/SimplifierWithPC.scala
@@ -94,7 +94,9 @@ trait SimplifierWithPC extends Transformer { self =>
     case e if path implies not(e) => (BooleanLiteral(false).copiedFrom(e), true)
 
     case c @ Choose(res, BooleanLiteral(true)) if hasInstance(res.tpe) == Some(true) => (c, true)
-    case c: Choose => (c, false)
+    case Choose(res, pred) =>
+      val (rp, _) = simplify(pred, path withBound res)
+      (Choose(res, rp).copiedFrom(e), false)
 
     case Lambda(params, body) =>
       val (rb, _) = simplify(body, path withBounds params)
