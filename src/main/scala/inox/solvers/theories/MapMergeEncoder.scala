59bd90b441e45224150acc16e0dc681520f3460a
Georg Stefan Schmid <georg.schmid@epfl.ch>
2020-11-26 16:44:16 +0100

    Simplify encoding in MapMergeEncoder a bit

--- src/main/scala/inox/solvers/theories/MapMergeEncoder.scala
+++ src/main/scala/inox/solvers/theories/MapMergeEncoder.scala
@@ -6,44 +6,36 @@ package theories
 
 trait MapMergeEncoder extends SimpleEncoder {
   import trees._
+  import trees.dsl._
 
-  object MapMergeEncoded {
-    def apply(e: MapMerge)(implicit s: Symbols): Expr = {
-      val MapMerge(mask, map1, map2) = e
-      val mapTpe @ MapType(keyTpe, _) = map1.getType
-      val (mapVd, keyVd) = (ValDef.fresh("map", mapTpe), ValDef.fresh("x", keyTpe))
-      val (mapV, keyV) = (mapVd.toVariable, keyVd.toVariable)
-      val cond = Equals(
-        MapApply(mapV, keyV).copiedFrom(e),
-        IfExpr(
-          ElementOfSet(keyV, mask).copiedFrom(e),
-          MapApply(map1, keyV).copiedFrom(e),
-          MapApply(map2, keyV).copiedFrom(e)).copiedFrom(e)).copiedFrom(e)
-      Choose(mapVd, Forall(Seq(keyVd), cond).copiedFrom(e)).copiedFrom(e)
-    }
-
-    def unapply(e: Expr)(implicit s: Symbols): Option[(Expr, Expr, Expr)] =
-      e match {
-        case Choose(mapVd, Forall(Seq(keyVd),
-            Equals(
-              MapApply(mapV1: Variable, keyV1: Variable),
-              IfExpr(
-                ElementOfSet(keyV2: Variable, mask),
-                MapApply(map1, keyV3: Variable),
-                MapApply(map2, keyV4: Variable)))))
-            if keyV1.id == keyVd.id && keyV2.id == keyVd.id &&
-               keyV3.id == keyVd.id && keyV4.id == keyVd.id &&
-               mapV1.id == mapVd.id =>
-          Some((mask, map1, map2))
-        case _ => None
-      }
+  val MapMergeID = FreshIdentifier("mapMerge")
+  val MapMergeFun = mkFunDef(MapMergeID)("K", "V") { case Seq(keyT, valueT) =>
+    val mapTpe = MapType(keyT, valueT)
+    (Seq("mask" :: SetType(keyT), "map1" :: mapTpe, "map2" :: mapTpe), mapTpe, {
+      case Seq(mask, map1, map2) =>
+        choose("map" :: mapTpe) { map =>
+          forall("x" :: keyT) { x =>
+            MapApply(map, x) ===
+            (if_ (ElementOfSet(mask, x)) {
+              MapApply(map1, x)
+            } else_ {
+              MapApply(map2, x)
+            })
+          }
+        }
+    })
   }
+
+  override val extraFunctions = Seq(MapMergeFun)
 
   protected object encoder extends SelfTreeTransformer {
     import sourceProgram._
 
     override def transform(e: Expr): Expr = e match {
-      case e: MapMerge => transform(MapMergeEncoded(e))
+      case MapMerge(mask, map1, map2) =>
+        val MapType(keyTpe, valueTpe) = map1.getType
+        MapMergeFun(transform(keyTpe), transform(valueTpe))(
+          transform(mask), transform(map1), transform(map2)).copiedFrom(e)
       case _ => super.transform(e)
     }
   }
@@ -52,7 +44,7 @@ trait MapMergeEncoder extends SimpleEncoder {
     import targetProgram._
 
     override def transform(e: Expr): Expr = e match {
-      case MapMergeEncoded(mask, map1, map2) =>
+      case FunctionInvocation(MapMergeID, _, Seq(mask, map1, map2)) =>
         MapMerge(transform(mask), transform(map1), transform(map2)).copiedFrom(e)
       case _ => super.transform(e)
     }
