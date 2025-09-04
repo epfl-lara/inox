efc9baf3457e35fcf6e1e85df049f17a2c370b8c
Sankalp Gambhir <sankalp.gambhir42@gmail.com>
2025-09-04 14:16:45 +0200

    Make install scripts curl only

--- scripts/install_sbt.sh
+++ scripts/install_sbt.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env sh
 
+CURL='curl -L -s -C - -O'
+
 SBT_VER="$(grep 'sbt.version' project/build.properties | cut -d= -f2)"
 if [ -z "$SBT_VER" ]; then
   echo "sbt.version not found in project/build.properties"
   exit 1
 fi
-wget -c https://github.com/sbt/sbt/releases/download/v${SBT_VER}/sbt-${SBT_VER}.tgz -q
+$CURL https://github.com/sbt/sbt/releases/download/v${SBT_VER}/sbt-${SBT_VER}.tgz
 tar xfz "sbt-${SBT_VER}.tgz"
--- scripts/install_solvers.sh
+++ scripts/install_solvers.sh
@@ -5,6 +5,13 @@ if [ "$INSTALL_SOLVERS" != "1" ]; then
   exit 0
 fi
 
+INFO_MSG="[INFO]"
+WARN_MSG="\033[33m[WARNING]\033[0m"
+ERROR_MSG="\033[1;31m[ERROR]\033[0m"
+
+# curl command with default flags
+CURL='curl -L -s -C -'
+
 TEMP_DIR="$(pwd)/temp"
 SOLVERS_DIR=${1:-"$(pwd)/solvers"}
 
@@ -26,40 +33,49 @@ CVC5_OS_NAME=$(uname -s | tr '[:upper:]' '[:lower:]' | sed 's/darwin/macOS/;s/li
 
 mkdir -p "$SOLVERS_DIR"
 mkdir -p "$TEMP_DIR"
+
+echo "$INFO_MSG Installing Solvers to $SOLVERS_DIR"
+
 # cvc5
-wget -c "https://github.com/cvc5/cvc5/releases/download/cvc5-${CVC5_VER}/cvc5-${CVC5_OS_NAME}-${ARCH}-static.zip" -O "$TEMP_DIR/cvc5.zip" -q
-unzip "$TEMP_DIR/cvc5.zip" -d "$TEMP_DIR" 
+echo "$INFO_MSG Installing cvc5 (v${CVC5_VER})"
+
+$CURL "https://github.com/cvc5/cvc5/releases/download/cvc5-${CVC5_VER}/cvc5-${CVC5_OS_NAME}-${ARCH}-static.zip" --output "$TEMP_DIR/cvc5.zip"
+unzip -q "$TEMP_DIR/cvc5.zip" -d "$TEMP_DIR" 
 CVC5_DIR=$(find "$TEMP_DIR" -mindepth 1 -maxdepth 1 -type d -name "*cvc5*")
 mv "$CVC5_DIR/bin/cvc5" "$SOLVERS_DIR/cvc5"
 chmod +x "$SOLVERS_DIR/cvc5"
 rm -rf "$TEMP_DIR"
 
 # CVC4
+echo "$INFO_MSG Installing CVC4 (v${CVC4_VER})"
+
 if [ "$CVC5_OS_NAME" = "macOS" ]; then
-  echo "[WARNING] CVC4 installation on macOS; defaulting to only build available (x86_64 v1.9 pre-release 25-09-2020)"
-  wget -c "https://cvc4.cs.stanford.edu/downloads/builds/macos/25-09-20/cvc4" -O "$SOLVERS_DIR/cvc4" -q
+  echo "$WARN_MSG CVC4 installation on macOS; defaulting to only build available (x86_64 v1.9 pre-release 25-09-2020)"
+  $CURL "https://cvc4.cs.stanford.edu/downloads/builds/macos/25-09-20/cvc4" -O "$SOLVERS_DIR/cvc4" -q
 elif [ "$CVC5_OS_NAME" = "Linux" ]; then
-  wget -c "https://cvc4.cs.stanford.edu/downloads/builds/x86_64-linux-opt/cvc4-${CVC4_VER}-x86_64-linux-opt" -O "$SOLVERS_DIR/cvc4" -q
+  $CURL "https://cvc4.cs.stanford.edu/downloads/builds/x86_64-linux-opt/cvc4-${CVC4_VER}-x86_64-linux-opt" --output "$SOLVERS_DIR/cvc4"
 else
-  echo "[ERROR] CVC4 installation not supported on unknown OS: $CVC5_OS_NAME"
+  echo "$ERROR_MSG CVC4 installation not supported on unknown OS: $CVC5_OS_NAME"
   exit 1
 fi
 chmod +x "$SOLVERS_DIR/cvc4"
 
 # z3
+echo "$INFO_MSG Installing Z3 (v${Z3_VER})"
+
 mkdir -p "$TEMP_DIR"
 Z3_FILE_PREFIX="z3-${Z3_VER}-${SHORT_ARCH}-${Z3_LIBC_NAME}"
 Z3_URL=$(
-  curl -s "https://api.github.com/repos/Z3Prover/z3/releases/tags/z3-${Z3_VER}" | # get release info
+  $CURL "https://api.github.com/repos/Z3Prover/z3/releases/tags/z3-${Z3_VER}" | # get release info
   grep "browser_download_url.*${Z3_FILE_PREFIX}" | #| # find url for the correct build
   sed 's/^.*: //;s/^"//;s/"$//' # strip non-url
 )
-wget -c "${Z3_URL}" -O "$TEMP_DIR/z3.zip" -q
-unzip "$TEMP_DIR/z3.zip" -d "$TEMP_DIR" 
+$CURL "${Z3_URL}" --output "$TEMP_DIR/z3.zip"
+unzip -q "$TEMP_DIR/z3.zip" -d "$TEMP_DIR" 
 Z3_DIR=$(find "$TEMP_DIR" -mindepth 1 -maxdepth 1 -type d -name "*z3*")
 mv "$Z3_DIR/bin/z3" "$SOLVERS_DIR/z3"
 chmod +x "$SOLVERS_DIR/z3"
 rm -rf "$TEMP_DIR"
 
-echo "************** Solvers Installed **************"
+echo "$INFO_MSG Solvers Installed"
 exit 0
