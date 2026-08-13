333907976ec8f808515acdb58aae5acc521f55cb
Sankalp Gambhir <sankalp.gambhir42@gmail.com>
2026-08-13 08:51:52 +0200

    Make portfolio wait on interrupts

--- src/main/scala/inox/solvers/combinators/PortfolioSolver.scala
+++ src/main/scala/inox/solvers/combinators/PortfolioSolver.scala
@@ -61,23 +61,29 @@ trait PortfolioSolver extends Solver { self =>
       }
     }
 
-    inox.utils.FutureUtils.findFirst(fs)(_._2 != Unknown) match {
-      case Some((s, r)) =>
-        resultSolver = s.getResultSolver
-        resultSolver.foreach { solv =>
-          reporter.debug("Solved with "+solv)
-        }
-        solvers.foreach(_.interrupt())
-        r
-      case None =>
-        reporter.debug("No solver succeeded")
-        config.cast(Unknown)
-    }
+    val res =
+      inox.utils.FutureUtils.findFirst(fs)(_._2 != Unknown) match {
+        case Some((s, r)) =>
+          resultSolver = s.getResultSolver
+          resultSolver.foreach { solv =>
+            reporter.debug("Solved with "+solv)
+          }
+          solvers.foreach(_.interrupt())
+          r
+        case None =>
+          reporter.debug("No solver succeeded")
+          config.cast(Unknown)
+      }
 
-    // TODO: Decide if we really want to wait for all the solvers.
-    // I understand we interrupt them, but what if one gets stuck
-    // fs foreach { Await.ready(_, Duration.Inf) }
-    // res
+    // @sg: wait for all the solvers to actually finish/be interrupted. This can
+    // otherwise cause an invalid prover state if we start another check while
+    // the previous one is in an unfinished state. In Princess for example, this
+    // would cause the next check to get completely stuck.
+    //
+    // We should not be waiting for more than 50ms~ in practice (polling time
+    // for Princess). 
+    fs.foreach(Await.ready(_, 100.milliseconds))
+    res
   }
 
 
